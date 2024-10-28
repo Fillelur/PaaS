9abfe1774d5f08106c28a444d367068db30101ae
filip johanson <Filip.johansson@iths.se>
2024-10-28 23:09:34 +0100

    + databas 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,10 @@
-import { useEffect } from 'react'
+//import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  let city = []
+  let city = "[]"
 
-  useEffect(() => {
-    fetch('/api')
-      .then((response) => response.json())
-      .then((result) => {
-        city = result.rows
-      })
-  }, [])
+
 
   return (
     <>
