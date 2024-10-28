ea0dc613ffd4c9b72b880a4a4f7cc2d04220d40c
filip johanson <Filip.johansson@iths.se>
2024-10-28 20:47:15 +0100

    + databas 1

--- backend/index.js
+++ backend/index.js
@@ -12,3 +12,28 @@ app.use(express.static(path.join(path.resolve(), 'dist')))
 app.listen(3000, () => {
   console.log('Redo på http://localhost:3000/')
 })
+
+
+
+const dotenv = require('dotenv'),
+  { Client } = require('pg')
+
+
+  dotenv.config()
+
+
+  const client = new Client({
+  connectionString: process.env.PGURI
+})
+
+client.connect()
+
+
+app.get('/api', async (_request, response) => {
+  const { rows } = await client.query(
+    'SELECT * FROM cities WHERE name = $1',
+    ['Stockholm']
+  )
+
+  response.send(rows)
+})
